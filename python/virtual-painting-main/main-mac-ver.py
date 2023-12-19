ae1fa50f75e3cf5a6a7643770d690f3cec9df48e
Lauren <lauren.thiel@hes-so.ch>
2023-12-19 09:58:56 +0100

    test day

--- python/virtual-painting-main/main-mac-ver.py
+++ python/virtual-painting-main/main-mac-ver.py
@@ -1,11 +1,10 @@
 import cv2
+import numpy as np
 import pygame
-from pygame.locals import *
-import os
+from pygame.locals import QUIT
 
-# Set the current working directory to the script's location
-script_dir = os.path.dirname(os.path.abspath(__file__))
-os.chdir(script_dir)
+circle_sensitivity = 50  # adjust this value for circle sensitivity, lower values make it more sensitive
+dark_color_sensitivity = 170  # adjust this value for dark color sensitivity
 
 # Initialize Pygame
 pygame.init()
@@ -19,10 +18,43 @@ screen_info = pygame.display.Info()
 width = screen_info.current_w
 height = screen_info.current_h
 
+x = width
+y = height
+
+
+def detect_dark_color(frame):
+    """
+    Detects dark colors in a frame.
+    """
+    frame_hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+
+    # Define the lower and upper bounds for dark colors (black)
+    lower_bound = np.array([0, 0, 0])
+    upper_bound = np.array([179, 255, dark_color_sensitivity])
+
+    # Create a mask for dark colors
+    mask = cv2.inRange(frame_hsv, lower_bound, upper_bound)
+    dark_contours, _ = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+    return dark_contours
+
+
+def draw_circle(frame, center, radius, color):
+    """
+    Draws a filled circle on the frame.
+    """
+    cv2.circle(frame, center, radius, color, thickness=cv2.FILLED)
+
+def draw_bordered_rectangle(frame, center, size, border_thickness, color):
+    """
+    Draws a bordered rectangle on the frame.
+    """
+    x, y = center
+    half_size = int(size / 2)
+    cv2.rectangle(frame, (x - half_size, y - half_size), (x + half_size, y + half_size), color, thickness=border_thickness)
+
 # Initialize the camera
 cap = cv2.VideoCapture(0)  # Use 0 for the default camera
-
-# Fetch the frame width and height
 frame_width = cap.get(cv2.CAP_PROP_FRAME_WIDTH)
 frame_height = cap.get(cv2.CAP_PROP_FRAME_HEIGHT)
 
@@ -30,63 +62,66 @@ frame_height = cap.get(cv2.CAP_PROP_FRAME_HEIGHT)
 ratio = frame_width / frame_height
 new_width = int(height * ratio)
 
-# Frame count initialization
-frame_number = 0
+# Circle parameters
+circle_radius_small = 20
+circle_radius_large = 50
 
-# Create 'frames' directory if it doesn't exist
-if not os.path.exists('frames'):
-    os.makedirs('frames')
+# Rectangle parameters
+rectangle_size = min(width, height) - 20  # 20 pixels smaller than the window size
+rectangle_border_thickness = 2
+rectangle_center = (int(width / 2), int(height / 2))
 
-# Function to save the frame
-def save_frame(image, directory='frames', prefix='frame', file_format='jpg'):
-    global frame_number  # Declare frame_number as global to modify it
-    filename = f"{prefix}_{frame_number}.{file_format}"
-    filepath = os.path.join(directory, filename)
-    cv2.imwrite(filepath, image)
-    print(f"Saved: {filepath}")
-    frame_number += 1  # Increment the frame number
+# Circle positions
+circle_center_top_left = (circle_radius_large, circle_radius_large)
+circle_center_top_right = (width - circle_radius_large, circle_radius_large)
 
-# Function to store the preview frame
-def store_frame(image, prefix='preview_frame', file_format='jpg'):
-    filename = f"{prefix}.{file_format}"  # Corrected line
-    cv2.imwrite(filename, image)
-    print(f"Saved: {filename}")
 
-try:
-    while True:
-        ret, frame = cap.read()
-        frame = cv2.resize(frame, (new_width, height))
-        if not ret:
-            print("Error capturing the frame.")
-            break
+while True:
+    ret, frame = cap.read()
+    frame = cv2.resize(frame, (new_width, height))
 
-        # Convert the OpenCV frame to Pygame surface
-        img = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-        img = pygame.surfarray.make_surface(img.swapaxes(0, 1))
+    # Detect dark colors
+    dark_contours = detect_dark_color(frame)
 
-        # Display the image on the screen
-        screen.blit(img, (0, 0))
-        pygame.display.flip()
+    # Draw a white circle at the top left with adjustable sensitivity
+    draw_circle(frame, circle_center_top_left, circle_radius_small, (255, 255, 255))
 
-        # Handle events
-        for event in pygame.event.get():
-            if event.type == QUIT or (event.type == KEYDOWN and event.key == K_q):
-                pygame.quit()
-                cap.release()
-                exit()
+    # Draw a white circle at the top right with adjustable sensitivity
+    draw_circle(frame, circle_center_top_right, circle_radius_small, (255, 255, 255))
 
-            # Check for 'y' key press to save the frame
-            if event.type == KEYDOWN and event.key == K_y:
-                save_frame(frame)  # Save the frame with an auto-incremented number
-                store_frame(frame)  # Save the preview frame with a fixed name
+    # Draw a bordered rectangle with black borders, centered
+    draw_bordered_rectangle(frame, rectangle_center, rectangle_size, rectangle_border_thickness, (0, 0, 0))
 
-            # Check for 'q' key press
-            keys = pygame.key.get_pressed()
-            if keys[K_q]:
-                pygame.quit()
-                cap.release()
-                exit()
+    img = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+    img = pygame.surfarray.make_surface(img.swapaxes(0, 1))
+    screen.blit(img, (0, 0))
+    pygame.display.flip()
+    print("playing video")
 
-finally:
-    # Release resources
-    cap.release()
+    # Check if dark colors are detected inside the circles with adjustable sensitivity
+    for contour in dark_contours:
+        M = cv2.moments(contour)
+        if M["m00"] > 0:
+            cx = int(M["m10"] / M["m00"])
+            cy = int(M["m01"] / M["m00"])
+
+            # Check if inside the top-left circle with adjustable sensitivity
+            if circle_center_top_left[0] - circle_sensitivity < cx < circle_center_top_left[0] + circle_sensitivity and \
+                    circle_center_top_left[1] - circle_sensitivity < cy < circle_center_top_left[1] + circle_sensitivity:
+                draw_circle(frame, circle_center_top_left, circle_radius_small, (0, 0, 255))
+
+            # Check if inside the top-right circle with adjustable sensitivity
+            elif circle_center_top_right[0] - circle_sensitivity < cx < circle_center_top_right[0] + circle_sensitivity and \
+                    circle_center_top_right[1] - circle_sensitivity < cy < circle_center_top_right[1] + circle_sensitivity:
+                draw_circle(frame, circle_center_top_right, circle_radius_small, (0, 0, 255))
+
+
+
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            pygame.quit()
+            exit()
+
+    if cv2.waitKey(1) == ord('q'):
+        # Quit
+        break
