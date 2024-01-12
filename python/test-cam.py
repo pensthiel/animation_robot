caf1a5b45d2e4c4626c7ca6eeadfdef20fe45fa8
Lauren <lauren.thiel@hes-so.ch>
2024-01-12 19:28:56 +0100

    finally picamera2 work ( it was not compatible with python virtul environment, not i gotta make it work with openCV

--- python/test-cam.py
+++ python/test-cam.py
@@ -1,8 +1,15 @@
+
+import cv2
 from picamera2 import Picamera2
-from libcamera import controls
-
-focus = 0.5
-
 picam2 = Picamera2()
-picam2.start(show_preview=True)
-picam2.set_controls({"AfMode": controls.AfModeEnum.Manual, "LensPosition": focus})
+picam2.preview_configuration.main.size = (1280,720)
+picam2.preview_configuration.main.format = "RGB888"
+picam2.preview_configuration.align()
+picam2.configure("preview")
+picam2.start()
+while True:
+    im= picam2.capture_array()
+    cv2.imshow("Camera", im)
+    if cv2.waitKey(1)==ord('q'):
+        break
+cv2.destroyAllWindows()
