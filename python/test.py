3ec6733dd4568d121a42a4e8d9a855610b0f0ff6
Lauren <84678253+pensthiel@users.noreply.github.com>
2024-05-04 23:05:17 +0200

    erased test

--- python/test.py
+++ python/test.py
@@ -1,20 +1,3 @@
 #!/usr/bin/python3
-import uhubctl
 
-uhubctl.utils.UHUBCTL_BINARY = "sudo /HEAD/local/bin/uhubctl"
-
-
-hubs = uhubctl.discover_hubs()
-
-for hub in hubs:
-    print(f"Found hub: {hub}")
-
-    for port in hub.ports:
-        print(f"   Found port: {port}")
-        
-        # You can use the optional argument `cached_results=False` for each of 
-        # these 3 methods in order to invalidate the internal cache,
-        # which is used for performance reasons
-        print(f"      Description: {port.description()}")
-        print(f"      Vendor ID: {port.vendor_id()}")
-        print(f"      Product ID: {port.product_id()}")
+#for testing purposes
