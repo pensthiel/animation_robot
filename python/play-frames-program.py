f36a60e104f8cd472d1cbec0722517c40e2ec704
Lauren <84678253+pensthiel@users.noreply.github.com>
2024-05-04 21:23:05 +0200

    play fames program + more clean up

---
+++ python/play-frames-program.py
@@ -0,0 +1,91 @@
+
+import os
+import time
+import pygame
+from pygame.locals import *
+
+zoom = 0.75
+offset_tweak_left = 160
+offset_tweak_top = -120
+
+pygame.mixer.pre_init()
+pygame.init()
+screen_size = (0, 0)
+screen = pygame.display.set_mode(screen_size, pygame.FULLSCREEN)
+screen_info = pygame.display.Info()
+width = screen_info.current_w
+height = screen_info.current_h
+
+script_dir = os.path.dirname(os.path.abspath(__file__))
+os.chdir(script_dir)
+
+reload_folder = os.path.join(script_dir, "reload")
+frame_number = 0
+
+for root_dir, cur_dir, files in os.walk(reload_folder):
+    frame_number += len(files)
+    frame_number -= 1
+
+print('File count in', reload_folder, ':', frame_number)
+
+frame_to_display = None
+filepath = None
+filepath2 = None
+preview_number = 0
+
+screen.fill((200, 150, 250))
+LEDS_on()
+try:
+    filename = f"frame_{frame_number}.jpg"
+    print(filename)
+    filepath = os.path.join("reload", filename)
+    print(filepath)
+    image = pygame.image.load(filepath)
+    scaled_image = pygame.transform.scale(image, (width, height))
+    screen.blit(scaled_image, (0, 0))
+    pygame.display.flip()
+except Exception as load_error:
+    print(f"Failed to load image: {load_error}")
+LEDS_on()
+
+try:
+    while True:
+        print("preview starts")
+        filepath2 = os.path.join("reload", f"frame_{preview_number}.jpg")
+        preview_number += 1
+        print(filepath2)
+        if os.path.exists(filepath2):
+            try:
+                if not image is None and not image.get_rect().size == (0, 0):
+                    image = pygame.image.load(filepath2)
+                    scaled_image = pygame.transform.scale(image, (width, height))
+                    print(filepath2 + " loaded")
+                    screen.blit(scaled_image, (0, 0))
+                    print(filepath2 + " displayed")
+                    time.sleep(0.1)
+            except Exception as file_error:
+                print("Error occurred while loading the image.")
+
+        else:
+            print("can't preview: Directory empty")
+            preview_button_pressed = False
+        try:
+            if preview_number == frame_number:
+                preview_number = 0
+                pygame.mixer.stop()
+                preview_button_pressed = False
+        except Exception as e:
+            print(f"error stopping preview: {e}")
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT or (event.type == pygame.KEYDOWN and event.key == pygame.K_q):
+                led_signal()
+                print("q to quit")
+                exit()
+
+except KeyboardInterrupt:
+    pass
+
+finally:
+    pygame.quit()
+    quit()
